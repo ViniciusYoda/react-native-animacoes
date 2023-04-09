f547e2f9a6425da937899f50a96e600b64781f75
Vinícius Yoda <vyoda4604@gmail.com>
2023-04-09 16:16:42 -0300

    animated

--- src/telas/Detalhes/index.js
+++ src/telas/Detalhes/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, ScrollView, Button } from 'react-native';
 import { TelaDeFundo } from '../../componentes/TelaDeFundo';
 import { InformacoesUsuario } from '../../componentes/InformacoesUsuario';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import mapa from '../../assets/mapa.png';
 import styles from './styles';
+import Animated, {useSharedValue, useAnimatedStyle, withSpring, withTiming, withRepeat } from 'react-native-reanimated';
 
 export default function Detalhes(props) {
   const dados = props.route.params;
+
+  const rotacao = useSharedValue(0);
+  const angulo = -30;
+  const [jaAnimou, setJaAnimou] = useState(false);
+
+  const estiloAnimado = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotate: `${rotacao.value}deg`
+        }
+      ]
+    }
+  })
+
+  function fazerRotacao() {
+    rotacao.value = withRepeat(withTiming(angulo, { duration: 120 }), 6, true)
+
+    setTimeout(() => {
+      setJaAnimou(true)
+    }, 1000)
+  }
 
   return (
     <TelaDeFundo>
@@ -15,6 +38,7 @@ export default function Detalhes(props) {
         showsVerticalScrollIndicator={false} 
         style={styles.container}
       >
+
         <InformacoesUsuario
           nome={dados.nome}
           detalhes="Cliente desde 2018"
@@ -47,13 +71,16 @@ export default function Detalhes(props) {
           <Text>{dados.endereco}</Text>
           <TouchableOpacity 
             style={styles.botao} 
+            onPress={fazerRotacao}
           >
             <Text style={styles.botaoTexto}>Notificar consulta</Text>
+            <Animated.View style={[styles.icone, estiloAnimado]}>
               <Icon 
-                name={'notifications-none'} 
+                name={ jaAnimou ? 'notifaction' : 'notifications-none'} 
                 size={20} 
                 color="#FFF"
               />
+            </Animated.View>
           </TouchableOpacity>
       </ScrollView>
     </TelaDeFundo>
--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { InformacoesUsuario } from "../../componentes/InformacoesUsuario";
 import { CardConsulta } from "../../componentes/CardConsulta";
@@ -6,31 +6,51 @@ import { TelaDeFundo } from "../../componentes/TelaDeFundo";
 import soniaFoto from "../../assets/sonia.png";
 import pacientes from "./pacientes";
 import styles from "./styles";
+import { CardConsultaShimmerEffect } from "../../componentes/CardConsultaShimmerEffect";
 
 export default function Principal({ navigation }) {
+  const [tempo, setTempo] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTempo(true)
+    }, 3000)
+  }, [])
 
   return (
     <TelaDeFundo>
-    <View style={styles.container}>
-      <InformacoesUsuario 
-        nome="Olá Sônia"
-        detalhes="Mais 4 consultas hoje"
-        foto={soniaFoto}
-      />
+      <View style={styles.container}>
+        <InformacoesUsuario
+          nome="Olá Sônia"
+          detalhes="Mais 4 consultas hoje"
+          foto={soniaFoto}
+        />
 
-      <Text style={styles.texto}>Hoje</Text>
+        <Text style={styles.texto}>Hoje</Text>
 
-      <FlatList
-        data={pacientes}
-        keyExtractor={item => String(item.id)}
-        renderItem={({ item }) => 
-        <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
-          <CardConsulta {...item} />
-        </TouchableOpacity>
+
+
+        {tempo ?
+          <FlatList
+            data={pacientes}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item }) =>
+              <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
+                <CardConsulta {...item} />
+              </TouchableOpacity>
+            }
+            showsVerticalScrollIndicator={false}
+          />
+          :
+          <>
+            <CardConsultaShimmerEffect />
+            <CardConsultaShimmerEffect />
+            <CardConsultaShimmerEffect />
+          </>
         }
-        showsVerticalScrollIndicator={false}
-      />
-    </View> 
+
+
+      </View>
     </TelaDeFundo>
   );
 }
